873e34e3b7d643eb5029072d106a5920b804c90c
urban-bit <gabrielgabriela57@gmail.com>
2024-08-28 18:23:03 +0200

    url script for relationship_app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,17 +1,14 @@
-# relationship_app/urls.py
-
 from django.urls import path
-from .views import list_books, BookListView, LibraryDetailView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    # URL pattern for the function-based view
-    path('books/', list_books, name='list_books'),
+    # User Authentication URLs
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 
-    # URL pattern for the class-based view
-    path('books/class/', BookListView.as_view(), name='book_list_class'),
-
-    # URL pattern for the LibraryDetailView
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    # Add more URL patterns as needed
+    # Other Views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
