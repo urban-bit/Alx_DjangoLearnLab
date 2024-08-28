d69b957c6f4b47fd4b53a98befe8ebb6f52bc4c5
urban-bit <gabrielgabriela57@gmail.com>
2024-08-28 19:22:48 +0200

    models.py updated with user profiles

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library
+from .forms import BookForm
 
 # Function-Based View to List All Books
 def list_books(request):
@@ -39,51 +40,46 @@ def is_member(user):
     return user.userprofile.role == 'Member'
 
 # Role-Based Views
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
-
-
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
-from .models import Book
-from .forms import BookForm
-
-@permission_required('django-models.can_add_book', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('list_books')
     else:
         form = BookForm()
-    return render(request, 'add_book.html', {'form': form})
+    return render(request, 'relationship_app/add_book.html', {'form': form})
 
-@permission_required('django-models.can_change_book', raise_exception=True)
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('list_books')
     else:
         form = BookForm(instance=book)
-    return render(request, 'edit_book.html', {'form': form})
+    return render(request, 'relationship_app/edit_book.html', {'form': form})
 
-@permission_required('django-models.can_delete_book', raise_exception=True)
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     if request.method == 'POST':
         book.delete()
-        return redirect('book_list')
-    return render(request, 'delete_book.html', {'book': book})
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
+
+# Role-Based Views (example)
+@permission_required('relationship_app.can_view_admin', raise_exception=True)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@permission_required('relationship_app.can_view_librarian', raise_exception=True)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@permission_required('relationship_app.can_view_member', raise_exception=True)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
