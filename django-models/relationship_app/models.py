3aaa5248c6bc08d509b8558ee151ba0c598d84f1
urban-bit <gabrielgabriela57@gmail.com>
2024-08-28 19:08:05 +0200

    models.py updated with define custome permissions

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -52,3 +52,25 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField()
+    cover = models.ImageField(upload_to='covers/')
+    language = models.CharField(max_length=30)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
