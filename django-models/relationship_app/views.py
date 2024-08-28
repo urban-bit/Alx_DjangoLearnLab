5edf7d9f567a31b489232d92b673719ca46135ab
urban-bit <gabrielgabriela57@gmail.com>
2024-08-28 18:51:48 +0200

    Set Up Role-Based Views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
 from .models import Book, Library
 
 # Function-Based View to List All Books
@@ -26,9 +27,8 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
 
+# Role Check Functions
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
@@ -38,14 +38,15 @@ def is_librarian(user):
 def is_member(user):
     return user.userprofile.role == 'Member'
 
+# Role-Based Views
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
