9cfaf5ccdad1110b92e153aaceb7a9a4de6ff063
urban-bit <gabrielgabriela57@gmail.com>
2024-08-28 18:40:45 +0200

    models.py updated with user profiles

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -41,3 +41,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
