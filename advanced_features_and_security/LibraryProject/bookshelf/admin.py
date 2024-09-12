aaa19fa8e135cbe9f0494b8fa99d7f48218b11bc
urban-bit <gabrielgabriela57@gmail.com>
2024-09-12 12:06:56 +0200

    bookshelf/admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,23 +1,20 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Book
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import Book
 
-# Custom User Admin
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
+# Create Groups
+editors, _ = Group.objects.get_or_create(name='Editors')
+viewers, _ = Group.objects.get_or_create(name='Viewers')
+admins, _ = Group.objects.get_or_create(name='Admins')
 
-# Book Admin
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
+# Get permissions
+content_type = ContentType.objects.get_for_model(Book)
+can_view = Permission.objects.get(codename='can_view', content_type=content_type)
+can_create = Permission.objects.get(codename='can_create', content_type=content_type)
+can_edit = Permission.objects.get(codename='can_edit', content_type=content_type)
+can_delete = Permission.objects.get(codename='can_delete', content_type=content_type)
 
-# Register the models with the admin site
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Book, BookAdmin)
+# Assign permissions to groups
+editors.permissions.add(can_create, can_edit)
+viewers.permissions.add(can_view)
+admins.permissions.add(can_view, can_create, can_edit, can_delete)
