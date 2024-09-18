d3507b208757c2478fbf58bb51ddfdd15c8999d1
urban-bit <gabrielgabriela57@gmail.com>
2024-09-18 15:00:46 +0200

    ../blog/views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from .forms import UserRegistrationForm, UserProfileForm
 
-# Create your views here.
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# User login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+# User logout view
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# User profile view
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = UserProfileForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
