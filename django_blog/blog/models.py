25bf123b7222a15d93fb2e333818ea8ba032faa5
urban-bit <gabrielgabriela57@gmail.com>
2024-10-02 20:21:55 +0200

    Resolved merge conflicts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,7 +5,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    class Meta:
+        ordering = ['-published_date']  # Order by published date descending
 
     def __str__(self):
-        return self.title
+        return self.title  # Return the title of the post when the object is printed
