4be594b56b6e7b590e301568caca53009d6a71a3
urban-bit <gabrielgabriela57@gmail.com>
2024-10-02 21:01:39 +0200

    Resolved merge conflicts

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]  # Make sure this directory exists
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
