ce2a78017cf488da9b945539c19475381e4b5e6b
urban-bit <gabrielgabriela57@gmail.com>
2024-10-02 22:23:44 +0200

    Resolved merge conflicts

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post  # Import your Post model
 
-# Register your models here.
+# Create a custom admin class to manage the Post model
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')  # Fields to display in the list view
+    list_filter = ('author', 'published_date')  # Filters for the list view
+    search_fields = ('title', 'content')  # Searchable fields
+    prepopulated_fields = {'slug': ('title',)}  # Automatically populate slug field from title
+
+# Register the Post model with the custom admin class
+admin.site.register(Post, PostAdmin)
