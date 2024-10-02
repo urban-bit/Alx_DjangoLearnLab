a95131476488e93e1bf0f9efc62e4ff5c1756919
urban-bit <gabrielgabriela57@gmail.com>
2024-10-02 19:05:13 +0200

    Resolved merge conflicts

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")  # Adjust this if your settings module is different
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +14,5 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
