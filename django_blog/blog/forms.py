3238b99b2c6256bd21e4b9acc26a6d209620d668
urban-bit <gabrielgabriela57@gmail.com>
2024-10-22 10:03:54 +0200

    update auth on forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -7,11 +7,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')  # Include email in the form fields
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.email = self.cleaned_data['email']  # Save the email field
         if commit:
             user.save()
         return user
